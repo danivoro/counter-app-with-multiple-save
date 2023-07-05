17c23e2f7aa42583ffbfeee7dfcf97f8d7ec9e8a
dani voro <mr.voro@gmail.com>
2023-07-05 17:33:02 +0200

    finishing the exercide

--- src/components/NumberPicker.tsx
+++ src/components/NumberPicker.tsx
@@ -1,31 +1,33 @@
 import { useState } from "react";
 
 export default function NumberPicker(): JSX.Element {
-  const [counterValueFromCurrentRender, queueRerenderWithNewCounterValue] =
-    useState(0);
-  const [storedValuesFromCurrentRender, queueRerenderWithNewStoredValues] =
-    useState<number[]>([]);
+  const [counterValue, setCounterValue] = useState(0);
+  const [storedValues, setStoredValues] = useState<number[]>([]);
 
   const handleAddOneToCounter = () => {
-    queueRerenderWithNewCounterValue(counterValueFromCurrentRender + 1);
+    setCounterValue(previousCount => previousCount + 1);
   };
 
   const handleSubtractOneFromCounter = () => {
-    queueRerenderWithNewCounterValue(counterValueFromCurrentRender - 1);
+    setCounterValue(previousCount => previousCount - 1);
   };
 
   const handleStoreCurrentCount = () => {
-    queueRerenderWithNewStoredValues([
-      ...storedValuesFromCurrentRender,
-      counterValueFromCurrentRender,
+    setStoredValues(prevValues => [...prevValues, counterValue
     ]);
   };
 
   return (
     <>
       <h1>Number picker</h1>
-      <p>Your stored numbers: {storedValuesFromCurrentRender.join(", ")}</p>
-      <p>Counter: {counterValueFromCurrentRender}</p>
+      <p>Your stored numbers:</p>
+      <ul>
+        {storedValues.slice(-5)
+          .map((number, index) => (
+          <li key={index}>{number}</li>
+        ))}
+      </ul>
+      <p>Counter: {counterValue}</p>
       <button onClick={handleSubtractOneFromCounter}>-1</button>
       <button onClick={handleAddOneToCounter}>+1</button>
       <hr />
